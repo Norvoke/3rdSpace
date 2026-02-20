150757a7d092e2e612513300c0e722849a6ba402
Norvoke <finnellingwood@gmail.com>
2026-02-20 14:26:44 -0700

    updated server index.ts to work for railway

--- server/src/index.ts
+++ server/src/index.ts
@@ -58,7 +58,7 @@ import path from 'path';
 
 // Serve React app in production
 if (process.env.NODE_ENV === 'production') {
-  const clientBuild = path.join(__dirname, '../../client/dist');
+  const clientBuild = path.join(__dirname, '../client/dist');
   app.use(express.static(clientBuild));
   app.get('*', (_req, res) => {
     res.sendFile(path.join(clientBuild, 'index.html'));
